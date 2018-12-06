12062befad55ff5e978cf34f71cc2bf75b29e488
Suman Chatterjee <chatterjeesuman@gmail.com>
2018-12-06 09:21:32 +0530

    Initial Commit

---
+++ TheMovieRx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieRx
+//
+//  Created by Suman Chatterjee on 06/12/2018.
+//  Copyright © 2018 Suman Chatterjee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
